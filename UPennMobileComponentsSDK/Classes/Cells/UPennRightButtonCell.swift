44b069af9eed5d30fe2550d75053618b19cb6a7e
Rashad Abdul-Salaam <upennmobile@gmail.com>
2021-07-21 17:12:40 -0400

    Add required missing files

---
+++ UPennMobileComponentsSDK/Classes/Cells/UPennRightButtonCell.swift
@@ -0,0 +1,24 @@
+//
+//  UPennRightButtonCell.swift
+//  UPennMobileComponentsSDK
+//
+//  Created by Rashad Abdul-Salam on 6/25/21.
+//
+
+import Foundation
+import UIKit
+
+public protocol UPennRightButtonDelegate : AnyObject {
+    func pressedRightButton(_ button: UIButton)
+}
+
+open class UPennRightButtonCell : UPennBasicCell, UPennRightButtonViewConfigureInterface {
+    
+    @IBOutlet weak var buttonView: UPennRightButtonView!
+    
+    
+    open func configure(title: String, styles: UPennButtonStyler?=nil, delegate: UPennRightButtonDelegate) {
+        self.buttonView.configure(title: title, styles: styles, delegate: delegate)
+    }
+}
+
