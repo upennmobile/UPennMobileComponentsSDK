4b42609a91a8bc46a74ae2d0e79452a3b570808a
Rashad Abdul-Salaam <upennmobile@gmail.com>
2021-12-27 10:54:42 -0500

    Update UPennApplicationSettings key for CurrentAppVersion Info-Dict

--- UPennMobileComponentsSDK/Classes/Utilities/UPennApplicationSettings.swift
+++ UPennMobileComponentsSDK/Classes/Utilities/UPennApplicationSettings.swift
@@ -18,7 +18,7 @@ open class UPennApplicationSettings {
     
     public static var Config = Bundle.main.object(forInfoDictionaryKey: "Config") as! String
     
-    public static var CurrentAppVersion = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
+    public static var CurrentAppVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString" as String) as! String
     
     public static var BundleID = Bundle.main.object(forInfoDictionaryKey: kCFBundleIdentifierKey as String) as! String
     
